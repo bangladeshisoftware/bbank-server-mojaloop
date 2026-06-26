b798fe430458563dd569b7e443322c44729a9492
developerAlihasan404439 <cao.bangladeshisoftware@gmail.com>
2026-06-27 01:04:26 +0600

    feat(payment): implement core p2p instant payment logic and mojaloop protocol phases

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**************************************************************************
+ * Copyright © 2026 Bangladeshi Software Ltd. All rights reserved.
+ * Distributed under the license terms specified in this repository.
+ *
+ * ORIGINAL AUTHOR: Muhammad Nasim (Developer)
+ **************************************************************************/
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
